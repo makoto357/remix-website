39681af51a4e64dc0a9b17331fdf19aa887d339a
clairechu <makoto357@gmail.com>
2023-11-28 00:32:13 +0800

    fetch data from api

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,10 +9,36 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+interface PhotoData {
+  id: number;
+  description: string;
+  url: string;
+  title: string;
+  user: number;
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const res = await fetch(
+    "https://api.slingacademy.com/v1/sample-data/photos"
+  ).then((res) => res.json());
+  return json<PhotoData[]>(res.photos);
+}
+
 export default function Index() {
+  const pictures = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="text-3xl font-bold mb-6">Notable Collection</h1>
-    </div>
+    <>
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+        <h1 className="text-3xl font-bold mb-6">Notable Collection</h1>
+      </div>
+      <div className="flex">
+        {pictures.map((p: PhotoData) => (
+          <div key={p.id}>
+            <img src={p.url} alt={p.title} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
